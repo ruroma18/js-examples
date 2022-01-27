74aec1c815db2555c70355ee84f34038cafd110c
Roman Rudenko <ruroma18@gmail.com>
2022-01-27 19:36:39 +0200

    Solve js function task

---
+++ index.js
@@ -0,0 +1,112 @@
+// const userName = prompt('Введите Ваше имя');
+// const exitPurpose = prompt('Вы уже уходите?')
+
+// const wordToCompareYes = 'да';
+
+// if (exitPurpose === wordToCompareYes) {
+//   alert('До встречи, ' + userName);
+// }
+// else {
+//   alert('Привет, ' + userName);
+// };
+
+
+// const myFirstFunction = function () {
+//   const userName = prompt('Введите Ваше имя');
+//   const exitPurpose = prompt('Вы уже уходите?')
+
+//   const wordToCompareYes = 'да';
+
+//   if (exitPurpose === wordToCompareYes) {
+//     alert('До встречи, ' + userName);
+//   }
+//   else {
+//     alert('Привет, ' + userName);
+//   }
+// }
+
+// myFirstFunction();
+
+// //является ли число четным
+// const evenOrOdd = function (num1) {
+//   if (isNaN(num1) ) {
+//     return 'ERROR. Please enter number';
+//   }
+
+//   if (num1 % 2 === 1) {
+//     return 'Odd number';
+//   }
+//   else {
+//     return 'Even number';
+//   }
+// }
+
+// const enterNumber = prompt('Please, enter number');
+// const result = evenOrOdd(enterNumber);
+// console.log(result);
+
+
+
+// // // возвращать наименьшее из двух чисел
+// const returnTheLeastNumber = function (num1, num2) {
+//   if (isNaN(num1) || isNaN(num2)) {
+//     return 'ERROR. Please enter number';
+//   }
+
+//   if (num1 > num2) {
+//     return num2;
+//   }
+//   else {
+//     return num1;
+//   }
+// }
+
+// const enterFirstNumber = prompt('Enter first number');
+// const enterSecondNumber = prompt('Enter Second number');
+// const result = returnTheLeastNumber(enterFirstNumber, enterSecondNumber);
+// console.log(result);
+
+
+// // // функцию которая принимает количестов денег у пользователя и стоимость товара
+// const moneyCalc = function (userMoney, costOfGoods) {
+
+//   if (isNaN(userMoney) || isNaN(costOfGoods)) {
+//     return 'ERROR. Please enter number';
+//   }
+
+//   if (userMoney >= costOfGoods) {
+//     return true;
+//   }
+//   else {
+//     return 'Sorry, You don`t have enough money';
+//   }
+// }
+
+// const enterUserMoney = prompt('Enter money, you have');
+// const enterCostOfGoods = prompt('Enter cost of goods');
+// const result = moneyCalc(enterUserMoney, enterCostOfGoods);
+// console.log(result);
+
+
+// // Создать функцию которая возвращает абсолютную величину числа
+// const absoluteNumValue = function (num) {
+//   if (isNaN(num)) {
+//     return 'ERROR. Please enter number';
+//   }
+//   if (num < 0) {
+//     return absoluteNum = -num;
+//   }
+//   else {
+//     return num
+//   }
+// }
+
+// const enterNum = prompt('Enter number');
+// result = absoluteNumValue(enterNum);
+// console.log(result);
+
+
+
+
+
+
