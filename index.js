e83a2d4c6c0ac864ecbf6504aa7753cec10bbb65
Roman Rudenko <ruroma18@gmail.com>
2022-01-28 18:40:51 +0200

    Refactoring

--- index.js
+++ index.js
@@ -1,79 +1,79 @@
-const trafficLight = function (trafficColor) {
-  if (trafficColor === 'red') {
-    console.log('Stop');
-  }
-  else if (trafficColor === 'yellow') {
-    console.log('Wait');
-  }
-  else if (trafficColor === 'green') {
-    console.log('Go')
-  }
-  else {
-    console.log('ERROR color')
-  }
-}
+// const trafficLight = function (trafficColor) {
+//   if (trafficColor === 'red') {
+//     return 'Stop';
+//   }
+//   else if (trafficColor === 'yellow') {
+//     return 'Wait';
+//   }
+//   else if (trafficColor === 'green') {
+//     return 'Go';
+//   }
+//   else {
+//     return 'ERROR color';
+//   }
+// }
 
-const enterTrafficColor = prompt('Enter traffic color');
-const result = trafficLight(enterTrafficColor);
-console.log(result);
+// const enterTrafficColor = prompt('Enter traffic color');
+// const result = trafficLight(enterTrafficColor);
+// console.log(result);
 
 
-const calculator = function (num1, num2, arithmeticOperator) {
-  switch (arithmeticOperator) {
-    case '+':
-      console.log(num1 + num2);
-      break;
-    case '-':
-      console.log(num1 - num2);
-      break;
-    case '*':
-      console.log(num1 * num2);
-      break;
-    case '/':
-      console.log(num1 / num2);
-      break;
-    default:
-      console.log('ERROR operator');
+// const calculator = function (num1, num2, arithmeticOperator) {
+//   switch (arithmeticOperator) {
+//     case '+':
+//       return num1 + num2;
+//       break;
+//     case '-':
+//       return num1 - num2;
+//       break;
+//     case '*':
+//       return num1 * num2;
+//       break;
+//     case '/':
+//       return num1 / num2;
+//       break;
+//     default:
+//       return 'ERROR operator';
+//   }
+// }
 
-  }
-}
-
-const enterNum1 = +prompt('Enter first number');
-const enterNum2 = +prompt('Enter second number');
-const enterArithmeticOperator = prompt('Enter arithmetic operator');
-result = calculator(enterNum1, enterNum2, enterArithmeticOperator);
-
+// const enterNum1 = +prompt('Enter first number');
+// const enterNum2 = +prompt('Enter second number');
+// const enterArithmeticOperator = prompt('Enter arithmetic operator');
+// result = calculator(enterNum1, enterNum2, enterArithmeticOperator);
+// console.log(result);
 
 const checkSeason = function (numberOfMonth) {
   switch (numberOfMonth) {
     case 12:
     case 1:
     case 2:
-      console.log('Winter');
+      return 'Winter';
       break;
     case 3:
     case 4:
     case 5:
-      console.log('Spring');
+      return 'Spring';
       break;
     case 6:
     case 7:
     case 8:
-      console.log('Summer');
+      return 'Summer';
       break;
     case 9:
     case 10:
     case 11:
-      console.log('Autumn');
+      return 'Autumn' ;
       break;
     default:
-      console.log('Use correct month number');
+      return 'Use correct month number';
       break;
   }
 }
 
 const enterMonthNumber = +prompt('Enter number of month');
 const result = checkSeason(enterMonthNumber);
+console.log(result);
 
 
 
